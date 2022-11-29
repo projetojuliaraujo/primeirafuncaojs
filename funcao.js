3f3e003a1bdf944b6d637d9f796a4b02654f929a
Júlia Araújo Nogueira de Freitas <100208717+projetojuliaraujo@users.noreply.github.com>
2022-11-29 08:55:05 -0300

    Add files via upload

--- funcao.js
+++ funcao.js
@@ -1,3 +1,37 @@
 function soma(a,b){
     return a+b
 }
+
+function subtr(a,b){
+    return a-b
+}
+function div(a,b){
+    return a/b
+}
+function multi(a,b){
+    return a*b
+}
+
+function decisao(resp, pri, seg){
+    if(resp == 1){
+        resultado = soma(pri, seg)
+        document.write("<p>A Soma é " + resultado)
+    }
+    if(resp == 2){
+        resultado = soma(pri, seg)
+        document.write("<p>A Subtração é " + resultado)
+
+    }
+    if(resp == 3){
+        resultado = soma(pri, seg)
+        document.write("<p>A Multiplicação é " + resultado)
+
+    }
+    if(resp == 4){
+        resultado = soma(pri, seg)
+        document.write("<p>A Divisão é " + resultado)
+
+    }    
+
+    
+}
